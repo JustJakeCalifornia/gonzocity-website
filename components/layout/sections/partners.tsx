873cf5083298b195cfee3dec8193bcf1102e416d
Nico <60318513+NicoMarksman@users.noreply.github.com>
2025-03-05 21:04:34 +0100

    implemented partners section

--- components/layout/sections/partners.tsx
+++ components/layout/sections/partners.tsx
@@ -1,37 +1,54 @@
 "use client"
 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel"
+import AutoScroll from "embla-carousel-auto-scroll"
+import { Icon } from "@/components/ui/icon"
+import { icons } from "lucide-react"
+
 interface partnersProps {
-  icon: string
+  id: number
+  image: string
   name: string
 }
 
 const partners: partnersProps[] = [
   {
-    icon: "Crown",
+    id: 1,
+    image: "Crown",
     name: "Acmebrand",
   },
   {
-    icon: "Vegan",
+    id: 2,
+    image: "Vegan",
     name: "Acmelogo",
   },
   {
-    icon: "Ghost",
+    id: 3,
+    image: "Ghost",
     name: "Acmesponsor",
   },
   {
-    icon: "Puzzle",
+    id: 4,
+    image: "Puzzle",
     name: "Acmeipsum",
   },
   {
-    icon: "Squirrel",
+    id: 5,
+    image: "Squirrel",
     name: "Acme",
   },
   {
-    icon: "Cookie",
+    id: 6,
+    image: "Cookie",
     name: "Accmee",
   },
   {
-    icon: "Drama",
+    id: 7,
+    image: "Drama",
     name: "Acmetech",
   },
 ]
@@ -62,6 +79,51 @@ export const PartnersSection = () => {
             across <span className="font-medium text-gray-900">50+ cities</span>{" "}
             worldwide
           </p> */}
+        </div>
+      </div>
+
+      <div className="pt-10 md:pt-16 lg:pt-20">
+        <div className="relative mx-auto flex items-center justify-center lg:max-w-5xl">
+          <Carousel
+            opts={{ loop: true }}
+            plugins={[AutoScroll({ playOnInit: true, speed: 1 })]}
+          >
+            <CarouselContent className="ml-0">
+              {partners.map((partner) => (
+                <CarouselItem
+                  key={partner.id}
+                  className="flex basis-1/3 justify-center pl-0 sm:basis-1/4 md:basis-1/5 lg:basis-1/6"
+                >
+                  <div className="mx-10 flex shrink-0 items-center justify-center">
+                    <div>
+                      <div
+                        key={partner.name}
+                        className="flex items-center text-xl md:text-2xl font-medium opacity-80 hover:opacity-100 transition-opacity"
+                      >
+                        <Icon
+                          name={partner.image as keyof typeof icons}
+                          size={32}
+                          color="black"
+                          className="mr-2"
+                        />
+                        {partner.name}
+                      </div>
+
+                      {/* <Image
+                        src={partner.image}
+                        alt={partner.description}
+                        className="h-4 w-auto"
+                        width={100}
+                        height={40}
+                      /> */}
+                    </div>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
+          <div className="absolute inset-y-0 left-0 w-12 bg-gradient-to-r from-background to-transparent"></div>
+          <div className="absolute inset-y-0 right-0 w-12 bg-gradient-to-l from-background to-transparent"></div>
         </div>
       </div>
     </section>
