b050708bd5a8c15a68c52c29dc822e4395d9cf1c
Nico <60318513+NicoMarksman@users.noreply.github.com>
2025-03-09 16:57:46 +0100

    i18n'd features  section

--- components/layout/sections/features.tsx
+++ components/layout/sections/features.tsx
@@ -1,64 +1,65 @@
-import {
-  IconCloud,
-  IconCurrencyDollar,
-  IconEaseInOut,
-  IconTerminal2,
-} from "@tabler/icons-react"
 import { FeatureGrid, FeatureItem } from "../grids/feature-grid"
-
-const items: FeatureItem[] = [
-  {
-    title: "Built for developers",
-    description:
-      "Built for engineers, developers, dreamers, thinkers and doers.",
-    icon: <IconTerminal2 />,
-  },
-  {
-    title: "Ease of use",
-    description:
-      "It's as easy as using an Apple, and as expensive as buying one.",
-    icon: <IconEaseInOut />,
-  },
-  {
-    title: "Pricing like no other",
-    description:
-      "Our prices are best in the market. No cap, no lock, no credit card required.",
-    icon: <IconCurrencyDollar />,
-  },
-  {
-    title: "100% Uptime guarantee",
-    description: "We just cannot be taken down by anyone.",
-    icon: <IconCloud />,
-  },
-  {
-    title: "100% Uptime guarantee",
-    description: "We just cannot be taken down by anyone.",
-    icon: <IconCloud />,
-  },
-  {
-    title: "100% Uptime guarantee",
-    description: "We just cannot be taken down by anyone.",
-    icon: <IconCloud />,
-  },
-]
+import {
+  BarChart3,
+  Bell,
+  ClipboardList,
+  PictureInPicture,
+  Sparkle,
+  TabletSmartphone,
+} from "lucide-react"
+import { useTranslations } from "next-intl"
 
 const FeaturesSection = () => {
+  const t = useTranslations("features")
+
+  const items: FeatureItem[] = [
+    {
+      title: t("items.item1.title"),
+      description: t("items.item1.description"),
+      icon: <ClipboardList />,
+    },
+    {
+      title: t("items.item2.title"),
+      description: t("items.item2.description"),
+      icon: <TabletSmartphone />,
+    },
+    {
+      title: t("items.item3.title"),
+      description: t("items.item3.description"),
+      icon: <BarChart3 />,
+    },
+    {
+      title: t("items.item4.title"),
+      description: t("items.item4.description"),
+      icon: <PictureInPicture />,
+    },
+    {
+      title: t("items.item5.title"),
+      description: t("items.item5.description"),
+      icon: <Sparkle />,
+    },
+    {
+      title: t("items.item6.title"),
+      description: t("items.item6.description"),
+      icon: <Bell />,
+    },
+  ]
+
   return (
     <section id="features" className="relative w-full py-20 overflow-hidden">
       <div className="container px-4 mx-auto">
         <div className="max-w-3xl mx-auto text-center">
-          {/* <h2 className="text-sm font-medium text-neutral-600">Features</h2> */}
-          <h2 className="text-sm font-medium text-brand">Features</h2>
+          {/* <h2 className="text-sm font-medium text-neutral-600">{t("sectionTitle")}</h2> */}
+          <h2 className="text-sm font-medium text-brand">
+            {t("sectionTitle")}
+          </h2>
 
           <h2 className="text-balance font-display text-3xl font-medium text-neutral-900 mb-3">
-            What Makes Us Different
+            {t("heading")}
           </h2>
 
           <p className="text-pretty text-lg text-neutral-500 leading-relaxed">
-            Transforming municipal efficiency by streamlining the documentation
-            of essential assets like gullies, trees, and street lights. Our
-            solutions empower local governments to manage their resources
-            effectively and enhance community services.
+            {t("description")}
           </p>
         </div>
       </div>
